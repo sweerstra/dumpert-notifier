1382063cb3dc4714dae356c802d1ea8c6f7a89f9
RussianDaddy <sjoerd.weerstra@hotmail.com>
2016-09-06 10:49:07 +0200

    Handled exception that occurred when connection was lost

--- DumpertNotifier/DumpertNotifier/DNForm.cs
+++ DumpertNotifier/DumpertNotifier/DNForm.cs
@@ -19,21 +19,38 @@ namespace DumpertNotifier
             InitializeComponent();
         }
 
+        //On balloon tip click, start default browser with new item link
         private void _notifyIcon_BalloonTipClicked(object sender, EventArgs e)
         {
             Process.Start((_manager.GetFirstUrl() ?? _homepage).ToString());
         }
 
+        //Every 10 seconds, if item has a new publish date, show a balloon tip with information
         private void _timer_Tick(object sender, EventArgs e)
         {
-            var lastItem = _manager.GetFirstItemFromFeed();
-            var lastUpdated = lastItem.PublishDate.DateTime;
-            if (lastUpdated <= _startTime) return;
+            var item = _manager.GetFirstItemFromFeed();
+            if (item != null)
+            {
+                var lastUpdated = item.PublishDate.DateTime;
+                if (lastUpdated <= _startTime) return;
 
-            _startTime = lastUpdated;
-            _notifyIcon.ShowBalloonTip(5000, "Nieuw filmpje!",
-                string.Format("{0}\n{1}\n{2}", lastItem.Title.Text, lastItem.Summary.Text,
-                    lastUpdated.ToShortTimeString()), ToolTipIcon.Info);
+                _startTime = lastUpdated;
+                _notifyIcon.ShowBalloonTip(5000, "Nieuw filmpje!",
+                    string.Format("{0}\n{1}\n{2}", item.Title.Text, item.Summary.Text,
+                        lastUpdated.ToShortTimeString()), ToolTipIcon.Info);
+            }
+            else
+            {
+                ConnectionInterrupted();
+            }
+        }
+
+        private void ConnectionInterrupted()
+        {
+            _notifyIcon.ShowBalloonTip(8000, "Verbinding verbroken!",
+                "Dumpert Notifier heeft geen internet verbinding kunnen vinden. Probeer opnieuw verbinding te maken en herstart de applicatie.",
+                ToolTipIcon.Error);
+            Close();
         }
 
         private void quitMenuItem_Click(object sender, EventArgs e)
