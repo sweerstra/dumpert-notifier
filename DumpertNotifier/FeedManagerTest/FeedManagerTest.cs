e82f70cd4e1365dbc54bd8e1964f9f3026f06592
RussianDaddy <sjoerd.weerstra@hotmail.com>
2016-09-06 12:18:30 +0200

    Added NUnit and tests for the FeedManager class

--- DumpertNotifier/FeedManagerTest/FeedManagerTest.cs
+++ DumpertNotifier/FeedManagerTest/FeedManagerTest.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using DumpertNotifier;
+using NUnit.Framework;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = NUnit.Framework.Assert;
 
 namespace ApplicationTest
 {
     [TestClass]
     public class FeedManagerTest
     {
-        
+        private readonly FeedManager _manager = new FeedManager();
+
+        [Test]
+        public void ExistingAndValidUrl()
+        {
+            var url = _manager.GetFirstUrl();
+            var correctUrl = url.AbsoluteUri.Contains("dumpert.nl/mediabase");
+            Assert.IsNotNull(url);
+            Assert.That(correctUrl, "Url was invalid and won't to redirect to video.");
+        }
+
+        [Test]
+        public void GetFirstItemFromValidFeed()
+        {
+            var item = _manager.GetFirstItemFromFeed();
+            Assert.IsNotNull(item.PublishDate);
+            Assert.That(item.PublishDate.DateTime < DateTime.Now, "DateTime was incorrectly formatted, can't be after the current time.");
+        }
+
+        [Test]
+        public void GetFeedFromValidUrl()
+        {
+            var feed = _manager.GetFeed(_manager.RssFeedUrl);
+            Assert.IsNotNull(feed.Items);
+        }
+
+        [Test]
+        public void NonexistingFeedUrl()
+        {
+            var exception = Assert.Catch(() => _manager.GetFeed(new Uri("www.invalidfeed.com")));
+            Assert.IsInstanceOf<UriFormatException>(exception, "Wrong Uri format wasn't handled correctly.");
+        }
     }
 }
