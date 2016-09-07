f9d6ce075d83becb4d3a0281f990bfc6fdafe542
RussianDaddy <sjoerd.weerstra@hotmail.com>
2016-09-07 14:54:40 +0200

    Removed some unused files

--- DumpertNotifier/DumpertNotifier/Program.cs
+++ DumpertNotifier/DumpertNotifier/Program.cs
@@ -6,6 +6,8 @@ namespace DumpertNotifier
 {
     internal static class Program
     {
+        private const string AppGuid = "c0a76b5a-12ab-45c5-b9d9-d693faa6e7b9";
+
         /// <summary>
         ///     The main entry point for the application.
         /// </summary>
@@ -14,16 +16,13 @@ namespace DumpertNotifier
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            using (Mutex mutex = new Mutex(false, "Global\\" + AppGuid))
+            using (var mutex = new Mutex(false, "Global\\" + AppGuid))
             {
-                if (!mutex.WaitOne(0, false))
+                if (mutex.WaitOne(0, false))
                 {
-                    MessageBox.Show(@"Instance already running");
-                    return;
+                    Application.Run(new DnForm());
                 }
-                Application.Run(new DnForm());
             }
         }
-        private const string AppGuid = "c0a76b5a-12ab-45c5-b9d9-d693faa6e7b9";
     }
 }
