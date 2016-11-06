31d1648c39c406acb68ec2f979213e4ddcf398b3
RussianDaddy <sjoerd.weerstra@hotmail.com>
2016-11-06 16:55:17 +0100

    Moved notifying to single method

--- src/DNForm.cs
+++ src/DNForm.cs
@@ -26,26 +26,20 @@ namespace DumpertNotifier
             var item = _manager.GetFirstItemFromFeed();
             if (item == null)
             {
-                ShowConnectionInterruptedAlert();
+                _notifyIcon.ShowBalloonTip(8000, "Verbinding verbroken!",
+                    "Dumpert Notifier heeft geen internet verbinding kunnen vinden. Probeer opnieuw verbinding te maken en herstart de applicatie.",
+                    ToolTipIcon.Error);
+                Close();
                 return;
             }
 
             var lastUpdated = item.PublishDate.DateTime;
             if (lastUpdated <= _startTime) return;
-            _startTime = lastUpdated;
 
+            _startTime = lastUpdated;
             _notifyIcon.ShowBalloonTip(5000, "Nieuw filmpje!",
                 $"{item.Title.Text}\n{item.Summary.Text}\n{lastUpdated.ToShortTimeString()}",
                 ToolTipIcon.Info);
-        }
-
-        private void ShowConnectionInterruptedAlert()
-        {
-            _notifyIcon.ShowBalloonTip(8000, "Verbinding verbroken!",
-                "Dumpert Notifier heeft geen internet verbinding kunnen vinden. Probeer opnieuw verbinding te maken en herstart de applicatie.",
-                ToolTipIcon.Error);
-
-            Close();
         }
 
         private void quitMenuItem_Click(object sender, EventArgs e)
